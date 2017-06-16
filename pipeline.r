c9d067a813fb96b1c09ca4696dcbcab1d8ca7575
Maxim Zavadskiy <maxim.zavadskiy@medisapiens.fi>
2017-06-16 17:06:58 +0300

    Add code for setting up bioconductor and downloading/extracting data

---
+++ pipeline.r
@@ -0,0 +1,13 @@
+# Install bioconductor
+source("https://bioconductor.org/biocLite.R")
+biocLite()
+
+# Install required libraries to work with the microarray  data of the given format
+biocLite("oligo")
+library(oligo)
+
+# Download and extract dataset
+download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE8524&format=file",destfile="micro-array-data.tar.gz")
+untar("micro-array-data.tar.gz",list=TRUE)
+
+# Next we will use oligo to import individual CEL files...
