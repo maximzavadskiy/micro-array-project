e4ece797654d000746276a826fbdd1d087e4ffa9
Maxim Zavadskiy <maxim.zavadskiy@medisapiens.fi>
2017-06-16 17:37:00 +0300

    Parse CEL data using oligo

--- pipeline.r
+++ pipeline.r
@@ -8,6 +8,9 @@ library(oligo)
 
 # Download and extract dataset
 download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE8524&format=file",destfile="micro-array-data.tar.gz")
-untar("micro-array-data.tar.gz",list=TRUE)
+untar("micro-array-data.tar.gz", list=TRUE)
 
 # Next we will use oligo to import individual CEL files...
+celFiles <- list.celfiles(".", listGzipped=TRUE)
+rawData <- read.celfiles(celFiles) # Magic! It will automatically detect the format of data and parse it for us!
+
