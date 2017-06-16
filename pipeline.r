e2631792cd592f80ec70115a5d7a29b4ff7ca3f3
Maxim Zavadskiy <maxim.zavadskiy@medisapiens.fi>
2017-06-16 18:03:47 +0300

    Print some useful information - TODO figure out meaning :D

--- pipeline.r
+++ pipeline.r
@@ -10,7 +10,14 @@ library(oligo)
 download.file("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE8524&format=file",destfile="micro-array-data.tar.gz")
 untar("micro-array-data.tar.gz", list=TRUE)
 
-# Next we will use oligo to import individual CEL files...
+# Next we will use oligo to import CEL files, see https://www.bioconductor.org/packages/release/bioc/vignettes/oligo/inst/doc/oug.pdf
 celFiles <- list.celfiles(".", listGzipped=TRUE)
 rawData <- read.celfiles(celFiles) # Magic! It will automatically detect the format of data and parse it for us!
 
+# Print first sequence. Letters!
+seq <- pmSequence(rawData)
+seq[1]
+
+# Print expression levels
+# Is this some relative gene expression levels? How to find for which genes?
+exprs(rawData) 
